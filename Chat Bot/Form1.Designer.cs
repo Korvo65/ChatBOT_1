6603ac82d9c410f788aee28ffb9a57f1a0ba7514
Korvo65 <55126971+Korvo65@users.noreply.github.com>
2020-04-08 16:42:56 +0900

    ДОбовление

---
+++ Chat Bot/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Chat_Bot
+{
+    partial class ChatForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChatForm));
+            this.ChatText = new System.Windows.Forms.TextBox();
+            this.QuestionText = new System.Windows.Forms.TextBox();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.ClearDialogButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ChatText
+            // 
+            this.ChatText.BackColor = System.Drawing.SystemColors.Window;
+            this.ChatText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ChatText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ChatText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(104)))), ((int)(((byte)(14)))));
+            this.ChatText.Location = new System.Drawing.Point(21, 12);
+            this.ChatText.Multiline = true;
+            this.ChatText.Name = "ChatText";
+            this.ChatText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ChatText.Size = new System.Drawing.Size(381, 224);
+            this.ChatText.TabIndex = 0;
+            // 
+            // QuestionText
+            // 
+            this.QuestionText.Location = new System.Drawing.Point(21, 255);
+            this.QuestionText.Multiline = true;
+            this.QuestionText.Name = "QuestionText";
+            this.QuestionText.Size = new System.Drawing.Size(258, 59);
+            this.QuestionText.TabIndex = 1;
+            // 
+            // SendButton
+            // 
+            this.SendButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(101)))), ((int)(((byte)(104)))));
+            this.SendButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SendButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.SendButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SendButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.SendButton.Location = new System.Drawing.Point(279, 255);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(123, 59);
+            this.SendButton.TabIndex = 2;
+            this.SendButton.Text = "Отправить (Enter)";
+            this.SendButton.UseVisualStyleBackColor = false;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // ClearDialogButton
+            // 
+            this.ClearDialogButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(101)))), ((int)(((byte)(104)))));
+            this.ClearDialogButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ClearDialogButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.ClearDialogButton.Location = new System.Drawing.Point(408, 255);
+            this.ClearDialogButton.Name = "ClearDialogButton";
+            this.ClearDialogButton.Size = new System.Drawing.Size(115, 31);
+            this.ClearDialogButton.TabIndex = 3;
+            this.ClearDialogButton.Text = "Очистить диалог";
+            this.ClearDialogButton.UseVisualStyleBackColor = false;
+            this.ClearDialogButton.Click += new System.EventHandler(this.ClearDialogButton_Click);
+            // 
+            // ChatForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(535, 326);
+            this.Controls.Add(this.ClearDialogButton);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.QuestionText);
+            this.Controls.Add(this.ChatText);
+            this.KeyPreview = true;
+            this.MaximumSize = new System.Drawing.Size(551, 365);
+            this.MinimumSize = new System.Drawing.Size(551, 365);
+            this.Name = "ChatForm";
+            this.Text = "Чат Бот";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ChatForm_FormClosed);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ChatForm_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ChatText;
+        private System.Windows.Forms.TextBox QuestionText;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.Button ClearDialogButton;
+    }
+}
+
---
+++ Chat Bot/LoginForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Chat_Bot
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.ExitLabel = new System.Windows.Forms.Label();
+            this.LoginLabel = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.EntryField = new System.Windows.Forms.TextBox();
+            this.MainPanel.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(101)))), ((int)(((byte)(104)))));
+            this.MainPanel.Controls.Add(this.ExitLabel);
+            this.MainPanel.Controls.Add(this.LoginLabel);
+            this.MainPanel.Cursor = System.Windows.Forms.Cursors.Default;
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.MainPanel.Location = new System.Drawing.Point(0, 0);
+            this.MainPanel.Margin = new System.Windows.Forms.Padding(4);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(380, 91);
+            this.MainPanel.TabIndex = 0;
+            this.MainPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainPanel_MouseDown);
+            this.MainPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainPanel_MouseMove);
+            // 
+            // ExitLabel
+            // 
+            this.ExitLabel.AutoSize = true;
+            this.ExitLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ExitLabel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.ExitLabel.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ExitLabel.ForeColor = System.Drawing.Color.White;
+            this.ExitLabel.Location = new System.Drawing.Point(319, 0);
+            this.ExitLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ExitLabel.Name = "ExitLabel";
+            this.ExitLabel.Size = new System.Drawing.Size(61, 21);
+            this.ExitLabel.TabIndex = 1;
+            this.ExitLabel.Text = "Выйти";
+            this.ExitLabel.Click += new System.EventHandler(this.ExitLabel_Click);
+            this.ExitLabel.MouseEnter += new System.EventHandler(this.ExitLabel_MouseEnter);
+            this.ExitLabel.MouseLeave += new System.EventHandler(this.ExitLabel_MouseLeave);
+            // 
+            // LoginLabel
+            // 
+            this.LoginLabel.Font = new System.Drawing.Font("Franklin Gothic Medium", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LoginLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.LoginLabel.Location = new System.Drawing.Point(5, 52);
+            this.LoginLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LoginLabel.Name = "LoginLabel";
+            this.LoginLabel.Size = new System.Drawing.Size(375, 54);
+            this.LoginLabel.TabIndex = 0;
+            this.LoginLabel.Text = "Введите свое имя:";
+            this.LoginLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.panel2.Controls.Add(this.LoginButton);
+            this.panel2.Controls.Add(this.EntryField);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 88);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(380, 257);
+            this.panel2.TabIndex = 1;
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(101)))), ((int)(((byte)(104)))));
+            this.LoginButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LoginButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.LoginButton.FlatAppearance.BorderSize = 2;
+            this.LoginButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LoginButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LoginButton.ForeColor = System.Drawing.Color.White;
+            this.LoginButton.Location = new System.Drawing.Point(74, 107);
+            this.LoginButton.Margin = new System.Windows.Forms.Padding(4);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(215, 47);
+            this.LoginButton.TabIndex = 1;
+            this.LoginButton.Text = "Авторизоваться";
+            this.LoginButton.UseVisualStyleBackColor = false;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // EntryField
+            // 
+            this.EntryField.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.EntryField.Location = new System.Drawing.Point(20, 36);
+            this.EntryField.Margin = new System.Windows.Forms.Padding(4);
+            this.EntryField.Multiline = true;
+            this.EntryField.Name = "EntryField";
+            this.EntryField.Size = new System.Drawing.Size(347, 37);
+            this.EntryField.TabIndex = 0;
+            this.EntryField.TextChanged += new System.EventHandler(this.EntryField_TextChanged);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 345);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.MainPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "LoginForm";
+            this.Text = "Авторизация";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.LoginForm_KeyDown);
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.Label LoginLabel;
+        private System.Windows.Forms.Label ExitLabel;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.TextBox EntryField;
+    }
+}
